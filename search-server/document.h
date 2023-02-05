2de737877e00e03fb1b954c05434e15ad0673054
UserUmbasa <117083294+UserUmbasa@users.noreply.github.com>
2023-02-05 16:01:12 +0300

    Update document.h

--- search-server/document.h
+++ search-server/document.h
@@ -26,13 +26,5 @@ struct DocumentData {
     int rating;
     DocumentStatus status;
 };
-struct QueryWord {
-    std::string data;
-    bool is_minus;
-    bool is_stop;
-};
-struct Query {
-    std::set<std::string> plus_words;
-    std::set<std::string> minus_words;
-};
+
 void PrintDocument(const Document& document);
