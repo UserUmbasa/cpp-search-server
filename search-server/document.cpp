af3665f0c7ef03735b992f1293a50fa9ab4abf98
UserUmbasa <117083294+UserUmbasa@users.noreply.github.com>
2023-02-05 10:21:14 +0300

    Create document.cpp

---
+++ search-server/document.cpp
@@ -0,0 +1,9 @@
+#include "document.h"
+#include  <iostream>
+
+void PrintDocument(const Document& document) {
+    std::cout <<"{ "
+        << "document_id = " << document.id << ", "
+        << "relevance = " << document.relevance << ", "
+        << "rating = " << document.rating << " }" << std::endl;
+}
