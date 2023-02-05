21733d390f9cdeac1b0dc828a3514a898bd7955c
UserUmbasa <117083294+UserUmbasa@users.noreply.github.com>
2023-02-05 10:36:07 +0300

    Create test_user.h

---
+++ search-server/test_user.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "read_input_functions.h"
+#include "search_server.h"
+#include <cstdlib> 
+#include "request_queue.h"
+
+
+void Manual_Input_String(); 
+void Machine_input_String();
+void Machine_input_Container();
+void Machine_input_EmptyRequest();
