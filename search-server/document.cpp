bb933df1b2ff6d857848b03e5020255d28817d4f
UserUmbasa <117083294+UserUmbasa@users.noreply.github.com>
2023-07-27 10:44:19 +0300

    Update document.cpp

--- search-server/document.cpp
+++ search-server/document.cpp
@@ -1,9 +1,1 @@
-#include "document.h"
-#include  <iostream>
 
-void PrintDocument(const Document& document) {
-    std::cout << "{ "
-        << "document_id = " << document.id << ", "
-        << "relevance = " << document.relevance << ", "
-        << "rating = " << document.rating << " }" ;
-}
